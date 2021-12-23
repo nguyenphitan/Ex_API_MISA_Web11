70aeb668a2fb565d2d1623b1173bf8493b2cf6bc
nguyenphitan <62367845+nguyenphitan@users.noreply.github.com>
2021-12-23 10:47:33 +0700

    Add files via upload

---
+++ MISA.Fresher.Web11/MISA.Fresher.Web11/Controllers/EmployeesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MISA.Fresher.Web11.Model;
+
+namespace MISA.Fresher.Web11.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        [HttpGet()]
+        public int? GetNumber(int? number, string name)
+        {
+            return number;
+        }
+
+        [HttpPost()]
+        public object GetName2([FromHeader] string accessToken, IList<Employee> employees, string? name)
+        {
+            return new {
+                Token = accessToken,
+                Name = name,
+                Data = employees
+            };
+        }
+
+        [HttpPut()]
+        public object UpdateEmployee(string? employeeCode, string? fullName)
+        {
+            return "";
+        }
+
+        [HttpDelete()]
+        public void DeleteEmployee(string? employeeCode)
+        {
+            
+        }
+
+
+    }
+}
---
+++ MISA.Fresher.Web11/MISA.Fresher.Web11/Model/Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace MISA.Fresher.Web11.Model
+{
+    public class Employee
+    {
+        public string EmployeeCode { get; set; }
+        public string FullName { get; set; }
+    }
+}
